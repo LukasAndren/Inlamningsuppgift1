e576068e362bcaee6226e9e0c7a14e37b1eba16b
buster <jlukasaa@gmail.com>
2018-10-01 13:51:13 +0200

    Second commit

---
+++ src/inlamningsuppgift1/Inlamningsuppgift1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package inlamningsuppgift1;
+
+/**
+ *
+ * @author buster
+ */
+public class Inlamningsuppgift1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("f");
+    
+    }
+
+}
